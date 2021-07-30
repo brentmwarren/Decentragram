9e6284ad445be577adb3808816f3e3ab42c35900
Brent Warren <65249561+brentmichael@users.noreply.github.com>
2021-07-29 23:59:53 -0700

    try

--- test/test.js
+++ test/test.js
@@ -25,4 +25,13 @@ contract("Decentragram", ([deployer, author, tipper]) => {
       assert.equal(name, "Decentragram");
     });
   });
+  describe("images", async () => {
+    let result;
+
+    it("creates images", async () => {
+      result = await decentragram.uploadImage();
+      let image = await decentragram.images(1);
+      console.log(images);
+    });
+  });
 });
